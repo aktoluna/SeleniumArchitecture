9821cf38659ca90a3330f9f38734f85eedfb1027
aAktolun <161992Oz5>
2017-11-26 01:18:20 +0300

    report category and author add and version BETA-1.3.2 released

--- slnarch-demo/src/test/java/com/saha/slnarch/demo/HomePageTest.java
+++ slnarch-demo/src/test/java/com/saha/slnarch/demo/HomePageTest.java
@@ -1,12 +1,16 @@
 package com.saha.slnarch.demo;
 
 import com.saha.slnarch.report.ExtentReportTest;
+import com.saha.slnarch.report.TestAuthor;
+import com.saha.slnarch.report.TestCategory;
 import org.junit.Test;
 
+@TestAuthor(authors = "Ali Aktolun")
 public class HomePageTest extends ExtentReportTest {
 
 
   @Test
+  @TestCategory(categories = "Demo")
   public void pageTest() throws Exception {
     HomePage homePage = new HomePage();
     homePage.pageTest();
