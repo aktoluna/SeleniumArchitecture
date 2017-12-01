3e96207568b8b3a4381bacabcb99ce4f015974d9
aAktolun <161992Oz5>
2017-12-01 10:13:51 +0300

    Script Util Add Nashorh

--- slnarch-common/src/main/java/com/saha/slnarch/common/ScriptUtil.java
+++ slnarch-common/src/main/java/com/saha/slnarch/common/ScriptUtil.java
@@ -2,37 +2,61 @@ package com.saha.slnarch.common;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.HashMap;
+import javax.script.Bindings;
 import javax.script.Invocable;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
+import javax.script.SimpleBindings;
 
 public enum ScriptUtil {
   INSTANCE;
+
+  private final String SCRIPT_ENGINE_NASHORN = "nashorn";
 
   public Object runFunction(String path, String functionName, Object args)
       throws FileNotFoundException, ScriptException, NoSuchMethodException {
     ScriptEngine scriptEngine = getDefaultScriptEngine();
     scriptEngine.eval(new FileReader(path));
     Invocable invocable = (Invocable) scriptEngine;
-    Object result = invocable.invokeFunction(functionName, args);
-    return result;
+    return invocable.invokeFunction(functionName, args);
   }
 
   public Object runScript(String script)
       throws ScriptException, NoSuchMethodException {
-    ScriptEngine scriptEngine = getDefaultScriptEngine();
-    Object result = scriptEngine.eval(script);
-//    Invocable invocable = (Invocable) scriptEngine;
-//    invocable.in
-    return result;
+    return getDefaultScriptEngine().eval(script);
   }
+
+  public Object runScript(String script, String key, Object value)
+      throws ScriptException, NoSuchMethodException {
+    Bindings bindings = new SimpleBindings();
+    bindings.put(key, value);
+    return getDefaultScriptEngine().eval(script, bindings);
+  }
+
+  public Object runScript(String script, HashMap<String, Object> bindingsMap)
+      throws ScriptException, NoSuchMethodException {
+    return getDefaultScriptEngine().eval(script, getBindingsByHashMap(bindingsMap));
+  }
+
+  public Bindings getBindingsByHashMap(HashMap<String, Object> bindingsMap) {
+    Bindings bindings = new SimpleBindings();
+    bindings.putAll(bindingsMap);
+    return bindings;
+  }
+
+  public Object runScript(String script, Bindings bindings)
+      throws ScriptException, NoSuchMethodException {
+    return getDefaultScriptEngine().eval(script, bindings);
+  }
+
 
   public ScriptEngineManager getScriptEngineManager() {
     return new ScriptEngineManager();
   }
 
   public ScriptEngine getDefaultScriptEngine() {
-    return getScriptEngineManager().getEngineByName("nashorn");
+    return getScriptEngineManager().getEngineByName(SCRIPT_ENGINE_NASHORN);
   }
 }
--- slnarch-common/src/test/java/com/saha/slnarch/common/ScriptUtilTest.java
+++ slnarch-common/src/test/java/com/saha/slnarch/common/ScriptUtilTest.java
@@ -1,8 +1,15 @@
 package com.saha.slnarch.common;
 
+import javax.script.Bindings;
+import javax.script.SimpleBindings;
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
 public class ScriptUtilTest {
+
+  @Test
+  public void runScript() throws Exception {
+  }
 
   @Test
   public void runFunctionByString() throws Exception {
@@ -13,6 +20,11 @@ public class ScriptUtilTest {
 
   @Test
   public void runFunction() throws Exception {
+    String script = "a < 5 ;";
+    Bindings bindings = new SimpleBindings();
+    bindings.put("a", 7);
+    boolean result = (boolean) ScriptUtil.INSTANCE.runScript(script, bindings);
+    Assertions.assertThat(result).isFalse();
   }
 
 }
