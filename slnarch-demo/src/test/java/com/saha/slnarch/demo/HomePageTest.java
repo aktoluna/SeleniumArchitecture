178ed9c4649fba1de8e61e4b4ab22a9576e2df1a
aAktolun <161992Oz5>
2017-12-07 21:53:49 +0300

    v1.4.0

--- slnarch-demo/src/test/java/com/saha/slnarch/demo/HomePageTest.java
+++ slnarch-demo/src/test/java/com/saha/slnarch/demo/HomePageTest.java
@@ -1,12 +1,14 @@
 package com.saha.slnarch.demo;
 
+import com.saha.slnarch.junit.InjectableJunitPageTestImpl;
+import com.saha.slnarch.junit.JunitPageTestImpl;
 import com.saha.slnarch.report.ExtentReportTest;
 import com.saha.slnarch.report.annotation.TestAuthor;
 import com.saha.slnarch.report.annotation.TestCategory;
 import org.junit.Test;
 
 @TestAuthor(authors = "Ali Aktolun")
-public class HomePageTest extends ExtentReportTest {
+public class HomePageTest extends InjectableJunitPageTestImpl {
 
   @Test
   @TestCategory(categories = "Demo")
